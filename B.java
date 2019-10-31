9a649bc97b2a66050938985dfe84b0df5904f533
Adrian H. S <48231255+Adrian51017001@users.noreply.github.com>
2019-10-31 12:04:11 +0700

    Add files via upload

---
+++ B.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percobaan3;
+
+/**
+ *
+ * @author ADRIAN H. SUWARDI
+ */
+public class B extends A {
+private int z;
+public void getJumlah() {
+System.out.println("Jumlah : "+ (x+y+z));
+}
+public void setZ(int z){
+this.z = z;}}
